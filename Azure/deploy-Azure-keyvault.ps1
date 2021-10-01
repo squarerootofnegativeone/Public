56114acc61927d20ecb91267d0237546ee640c32
Dave <david.blank@outlook.com>
2021-10-01 15:07:35 +1000

    Addedd comments

--- Azure/deploy-Azure-keyvault.ps1
+++ Azure/deploy-Azure-keyvault.ps1
@@ -1,13 +1,21 @@
+# Deploy KeyVault with randomised name to a Resource Group
+# Expectation is that this will be used in a Cloud Shell
+
+# Define a parameter to store Resource Group where KeyVault is to be deployed
 param (
     [Parameter(Mandatory=$true)]
-    [string]$ResourceGroup,
-
-    [Parameter(Mandatory=$true)]
-    [string]$location
+    [string]$ResourceGroup
 )
 
+# Get location of RG - dunno is this is strictly necessary for New-AzKeyVault
+$rg = Get-AzResourceGroup -Name $ResourceGroup
+
+# Generate random alphanumeric string with length of $length
 $length = 16
 $rnd = ""; do { $rnd = $rnd + ((0x30..0x39) + (0x41..0x5A) + (0x61..0x7A) | Get-Random | ForEach-Object {[char]$_}) } until ($rnd.length -eq $length)
+
+# Concatenate kv and random string to generate name for new Key Vault
 $kvname = "kv"+$rnd
 
-New-AzKeyvault -name $kvname -ResourceGroupName $ResourceGroup -Location $location -EnabledForDiskEncryption
+# No prizese for guessing what this cmdlet does...
+New-AzKeyvault -name $kvname -ResourceGroupName $ResourceGroup -Location $rg.location -EnabledForDiskEncryption
