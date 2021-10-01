ca8e88d846d9b18a61d018485f19b0b2d92db710
Dave <david.blank@outlook.com>
2021-10-01 15:52:27 +1000

    Added Connect-AzAccount

--- Azure/deploy-Azure-keyvault.ps1
+++ Azure/deploy-Azure-keyvault.ps1
@@ -1,11 +1,13 @@
 # Deploy KeyVault with randomised name to a Resource Group
-# Expectation is that this will be used in a Cloud Shell
 
 # Define a parameter to store Resource Group where KeyVault is to be deployed
 param (
     [Parameter(Mandatory=$true)]
     [string]$ResourceGroup
 )
+
+# Connect to Azure
+Connect-AzAccount -TenantId <# Tenant ID #> -SubscriptionId <# Subscrition ID #>
 
 # Get location of RG - dunno is this is strictly necessary for New-AzKeyVault
 $rg = Get-AzResourceGroup -Name $ResourceGroup
