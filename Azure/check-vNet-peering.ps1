741c1015321bd883bed0ef4f5c81b8fe90bf9d4f
Dave <david.blank@outlook.com>
2021-10-07 17:27:34 +1100

    added control loop

--- Azure/check-vNet-peering.ps1
+++ Azure/check-vNet-peering.ps1
@@ -1,3 +1,10 @@
-Connect-AzContext -TenantID '59aab5f9-7fdb-4dfd-89dd-0f4a2651f587'
+#Connect to VicPol Azure tenant and print all VNet peers for subscriptions starting with "vicpol"
+Connect-AzAccount -TenantId '59aab5f9-7fdb-4dfd-89dd-0f4a2651f587'
 
-Get-AzVirtualNetwork 
+$sublist = Get-AzSubscription -TenantId '59aab5f9-7fdb-4dfd-89dd-0f4a2651f587'
+foreach ($sub in $sublist) {
+     if ($sub.name -like 'vicpol*') {
+        Write-Output 'Subscription Name:' $sub.name
+        Get-AzVirtualNetwork | Select-Object Name, VirtualNetworkPeerings
+        }
+}
